626ff3ae00f006ec7aba68b96230a0e02e6f2013
Edward Francis <edwardafrancis@hotmail.com>
2018-12-16 19:20:13 +0000

    added trafik info url parameter

--- app/lib/TrafikInfo.pm
+++ app/lib/TrafikInfo.pm
@@ -9,5 +9,8 @@ has 'authentication_key' => sub {
     $ENV{AUTENTICATION_KEY} or croak 'Authentication key is required.';
 };
 
+has 'trafik_info_url' => sub {
+    $ENV{TRAFIK_INFO_URL} or croak 'Trafik Info URL is required.';
+};
 
 1;
