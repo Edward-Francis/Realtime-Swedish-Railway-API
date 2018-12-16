b4c5a15181ada5f8ce98367e3c087ff5eb75c0dd
Edward Francis <edwardafrancis@hotmail.com>
2018-12-16 19:09:57 +0000

    added required authentication key setter

--- app/lib/TrafikInfo.pm
+++ app/lib/TrafikInfo.pm
@@ -1,3 +1,13 @@
 package TrafikInfo;
 
+use Carp;
+use Mojo::Base -base;
+
+# ATTRIBUTES
+
+has 'authentication_key' => sub {
+    $ENV{AUTENTICATION_KEY} or croak 'Authentication key is required.';
+};
+
+
 1;
