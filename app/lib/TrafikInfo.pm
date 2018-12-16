2c6cebe2c3cca019620f83b2b9afca1813e16937
Edward Francis <edwardafrancis@hotmail.com>
2018-12-16 19:51:15 +0000

    added basic make request method

--- app/lib/TrafikInfo.pm
+++ app/lib/TrafikInfo.pm
@@ -1,7 +1,10 @@
 package TrafikInfo;
 
+# IMPORTS
 use Carp;
+use Mojo::UserAgent ();
 use Mojo::Base -base;
+
 
 # ATTRIBUTES
 
@@ -12,5 +15,28 @@ has 'authentication_key' => sub {
 has 'trafik_info_url' => sub {
     $ENV{TRAFIK_INFO_URL} or croak 'Trafik Info URL is required.';
 };
+
+
+# PRIVATE METHODS
+
+sub _make_request {
+    my ( $self, $query ) = @_;
+
+    # create payload
+    my $login = sprintf '<LOGIN authenticationkey="%s"/>', $self->authentication_key;
+    my $payload = sprintf '<REQUEST>%s%s</REQUEST>', $login, $query;
+
+    # make request
+    my $ua = Mojo::UserAgent->new;
+    my $tx = $ua->post( $self->trafik_info_url => $payload );
+    my $res = $tx->res;
+
+    if ( $res->is_error ) {
+        croak sprintf 'Request failed (%s): %s',
+            $res->code, $res->body;
+    }
+
+    return $res->json;
+}
 
 1;
